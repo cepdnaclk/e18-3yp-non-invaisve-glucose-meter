a73a8606615593c60b07e517c20aaacf390657cc
denuwan55 <ldweerarathne5555@gmail.com>
2022-12-19 20:07:12 +0530

    catchError added to checkAuth

--- code/WebApp/Backend/routes/userAuth.js
+++ code/WebApp/Backend/routes/userAuth.js
@@ -1,4 +1,4 @@
-const router = require("express").Router();
+  const router = require("express").Router();
 const User = require("../models/Patient");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
