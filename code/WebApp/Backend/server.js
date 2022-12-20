589c90dcd93fa8f18717a08d57ce515849d209eb
denuwan55 <ldweerarathne5555@gmail.com>
2022-12-20 13:03:27 +0530

    minor route change

--- code/WebApp/Backend/server.js
+++ code/WebApp/Backend/server.js
@@ -9,7 +9,7 @@
 const express = require("express");
 const colors = require("colors");
 const cors = require("cors");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv").config();
 // const db = require("./configurations/db");
 const connectDB = require("./config/db");
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: false }));
 // admin routes
 app.use("/api/admin", require("./routes/adminRoutes.js"));
 // user auth routes
-// app.use("/api/auth", require("./routes/userAuth"));
+app.use("/api/user", require("./routes/userAuth"));
 app.use("/api/auth", require("./routes/authRoutes"));
 // measurement routes
 app.use("/api/glucose", require("./routes/measurementRoutes"));
